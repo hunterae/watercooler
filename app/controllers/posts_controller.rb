2e6b6df62df0dd1833d2fe78e411ba8db417253f
Andrew Hunter <andrew.hunter@livingsocial.com>
2015-11-10 12:30:36 -0700

    Generated devise views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
+  end
+
+  def new
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts, only: [:index, :create] do
+  resources :posts, only: [:index, :new, :create] do
     member do
       put :like
       put :dislike
